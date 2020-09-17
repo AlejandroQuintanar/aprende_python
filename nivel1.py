5060284f0d31e5d950f06d571d31c900958f7bf8
AlejandroQuintanar <71471961+AlejandroQuintanar@users.noreply.github.com>
2020-09-17 18:53:19 -0500

    Create nivel1.py

---
+++ nivel1.py
@@ -0,0 +1,10 @@
+#Primer nivel del juego, sacar tres valores de la entrada y ponerlos en la salida
+from bandeja import Bandeja
+
+bandeja=Bandeja(1,5,"a")
+x=bandeja.entrada()
+bandeja.salida (x)
+y=bandeja.entrada()
+bandeja.salida(y)
+z=bandeja.entrada()
+bandeja.salida(z)
