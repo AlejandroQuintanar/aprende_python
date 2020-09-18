21062f365d9fee7fd4e7d3a137871b70f07ed29d
AlejandroQuintanar <71471961+AlejandroQuintanar@users.noreply.github.com>
2020-09-17 19:01:46 -0500

    Create bandeja.py

---
+++ bandeja.py
@@ -0,0 +1,21 @@
+# Este programa intenta emular la funcionalidad de entrada (input) y salida (output) como en el juego
+# Human Resource Machine.
+
+class Bandeja:
+    def __init__(self, *elementos):
+        self.elementos = elementos [::-1]
+        self.copia=list(elementos)
+        self.resultados = []
+
+    def entrada(self):
+        # Saca un elemento de la "entrada" como en el juego
+        return self.elementos.pop()
+
+
+    def salida(self, cosa):
+        # Pone un elemento en la "salida" como en el juego, no hay valor de "return"
+        print(cosa)
+        self.resultados.append(cosa)
+
+    def verificar(self):
+        pass
