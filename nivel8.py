42b9b3b958e16b742548791988d1eef015ff1d9e
AlejandroQuintanar <71471961+AlejandroQuintanar@users.noreply.github.com>
2020-10-05 14:49:46 -0500

    nivel8 listo

--- nivel8.py
+++ nivel8.py
@@ -1,25 +1,10 @@
-#Envia a la salida solo lo que sea cero.
-#Para comparar un numero con otro o una variable con un numero o dos
-#variables entre si, se usa el operador ==.
-#El operador == de un lado debe de tener una cosa y del otro lado debe de tener
-#la otra cosa1==cosa2
-#El comparar una cosa con otra y preguntar si ambas son iguales puede ser
-#verdad o no, en python se reprsenta la verdad como True y cuando es falso
-#se representa con False. A estros valores se les llaman buleanos.
-#En python el jump if zero se escribe con if buleano:
-#si buleano es verdadero se ejecuta el codigo que este indentado debajo del if
-# a ese codigo se le conoce como el cuerpo del if.
-#Un modo de escribirlo seria :
-#buleano=baldosa==0
-#if buleano:
-#bandeja.salida(baldosa)
+#Debemos tomar los numeros de la bandeja de entrada tripicarlos y sacarlos a
+#la bandeja de salida.
 
 from bandeja import *
-bandeja=Bandeja(1,9,5,0,3,7,0,4)
+bandeja=Bandeja (0,1,2,3,4,5)
 while not bandeja.entrada_vacia():
     baldosa=bandeja.entrada()
-    if baldosa==0:
-        bandeja.salida(baldosa)
+    baldosa1=baldosa+baldosa+baldosa
+    bandeja.salida(baldosa1)
     
-
-   
